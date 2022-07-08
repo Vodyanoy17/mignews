0ef7a009b8c39bf383196a5ab202926f19954628
Gregory Vinopal <gvinopal@hotmail.com>
2022-07-08 14:42:44 +0300

    eeee

--- test_dir/stepik211.py
+++ test_dir/stepik211.py
@@ -2,7 +2,7 @@
 """
 
 
-class PointDir:
+class List:
     """_summary_"""
 
     def __init__(self, x_coord, y_coord):
@@ -12,12 +12,3 @@ class PointDir:
             x_coord ([type]): [description]
             y_coord ([type]): [description]
         """
-        self.__x_coord = x_coord
-        self.__y_coord = y_coord
-    def get_point(self):
-        """Return the point"""
-        return (self.__x_coord, self.__y_coord)
-    def set_coord(self, x_coord, y_coord):
-        """Set the coordinates of the point"""
-        self.__x_coord = x_coord
-        self.__y_coord = y_coord
