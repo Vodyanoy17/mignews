c88d58a0e946ef287a30205a0c733fac6f826844
Gregory Vinopal <gvinopal@hotmail.com>
2022-07-08 18:37:42 +0300

    fix:comments

--- stepik211.py
+++ stepik211.py
@@ -12,9 +12,8 @@ class Point123:
 
     def __init__(self, x_coord, y_coord):
         """AI  the coordinates of the point.
-
         Args:
-            x_coord ([type]): [description]
+            x_coord : [description]
             y_coord ([type]): [description]
         """
         self.__x_coord = x_coord
