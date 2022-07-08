16622f195e663555e20fca314433362d1206a48f
Gregory Vinopal <gvinopal@hotmail.com>
2022-07-08 14:13:37 +0300

    fix pylint localy

--- stepik211.py
+++ stepik211.py
@@ -1,4 +1,19 @@
+"""Point that the given point is considered
+"""
+
+
 class Point:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+    """_summary_"""
+
+    def __init__(self, x_coord, y_coord):
+        """AI  the coordinates of the point.
+
+        Args:
+            x_coord ([type]): [description]
+            y_coord ([type]): [description]
+        """
+        self.__x_coord = x_coord
+        self.__y_coord = y_coord
+    def get_point(self):
+        """Return the point"""
+        return (self.__x_coord, self.__y_coord)
