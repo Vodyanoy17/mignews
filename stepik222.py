492292f7178b9f4c11b265557915c02e454194fb
Gregory Vinopal <gvinopal@hotmail.com>
2022-07-09 15:36:17 +0300

    ci(fix tag method):

--- stepik222.py
+++ stepik222.py
@@ -1,5 +1,6 @@
 """AI is creating summary for 
 """
+from re import T
 from string import ascii_lowercase,digits
 
 class CardCheck:
@@ -10,12 +11,18 @@ class CardCheck:
         """AI is creating summary for check_card_number
 
         Args:
-            number ([type]): [description]
+            number (str): Формат номера следующий: XXXX-XXXX-XXXX-XXXX,
+            где X - любая цифра (от 0 до 9).
         """
+        import re
+        p = re.compile(r'^(\d{4}-){3}(\d{4})$')
+        
+        re.search(p, number)
     def check_name(self,name):
         """AI is creating summary for check_name
 
         Args:
-            name ([type]): [description]
+            name ([str]):  проверяет строку name именем пользователя карты.
+            Возвращает булево значение True, если имя записано верно и False - в противном случае
         """
-    
+        return all(c in self.CHARS_FOR_NAME for c in name)
