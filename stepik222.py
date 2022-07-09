bace4ca2e668c9dca6c811a08133bea6bdba05fa
Gregory Vinopal <gvinopal@hotmail.com>
2022-07-09 15:15:33 +0300

    feat(new stefpik file and class):

---
+++ stepik222.py
@@ -0,0 +1,21 @@
+"""AI is creating summary for 
+"""
+from string import ascii_lowercase,digits
+
+class CardCheck:
+    '''A CardCheck class that checks whether a card is valid'''
+    CHARS_FOR_NAME = ascii_lowercase.upper() + digits
+
+    def check_card_number(self, number):
+        """AI is creating summary for check_card_number
+
+        Args:
+            number ([type]): [description]
+        """
+    def check_name(self,name):
+        """AI is creating summary for check_name
+
+        Args:
+            name ([type]): [description]
+        """
+    
