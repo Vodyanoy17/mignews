87cd812c498a5bff2508c0ca78fa622814c6be8a
Gregory Vinopal <gvinopal@hotmail.com>
2022-07-09 15:42:47 +0300

    feat(add new class CardCheck): BREAKING CHANGE:

--- stepik222.py
+++ stepik222.py
@@ -1,28 +1,36 @@
 """AI is creating summary for 
 """
-from re import T
-from string import ascii_lowercase,digits
+import re
+from string import ascii_lowercase, digits
+
 
 class CardCheck:
-    '''A CardCheck class that checks whether a card is valid'''
+    """A CardCheck class that checks whether a card is valid"""
+
     CHARS_FOR_NAME = ascii_lowercase.upper() + digits
 
-    def check_card_number(self, number):
+    @classmethod
+    def check_card_number(cls, number):
         """AI is creating summary for check_card_number
 
         Args:
             number (str): Формат номера следующий: XXXX-XXXX-XXXX-XXXX,
             где X - любая цифра (от 0 до 9).
         """
-        import re
-        p = re.compile(r'^(\d{4}-){3}(\d{4})$')
-        
-        re.search(p, number)
-    def check_name(self,name):
+        pattern = re.compile(r"^(\d{4}-){3}(\d{4})$")
+
+        if re.search(pattern, number):
+            return True
+        else:
+            return False
+
+    @classmethod
+    def check_name(cls, name):
         """AI is creating summary for check_name
 
         Args:
             name ([str]):  проверяет строку name именем пользователя карты.
             Возвращает булево значение True, если имя записано верно и False - в противном случае
         """
-        return all(c in self.CHARS_FOR_NAME for c in name)
+        name_tmp = name.replace(" ", "")
+        return all(c in cls.CHARS_FOR_NAME for c in name_tmp)
